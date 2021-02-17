dfc7fc7fc2e1de0184786647128448b82590cfb5
student <student@eda-bc-11.localdomain>
2021-02-18 11:58:49 +1300

    add join to getMeeting function

--- server/db/meetings.js
+++ server/db/meetings.js
@@ -1,33 +1,42 @@
-const connection = require('./connection')
+const connection = require("./connection");
 
-function getMeetings (db = connection) {
-  return db('meetings')
-    .select()
-} 
-
-function getMeeting (id, db = connection) {
-  return db('meetings')
-    .where('id', id)
-    .select()
-    .first()
+function getMeetings(db = connection) {
+  return db("meetings").select();
 }
 
-function addMeeting (meeting, db = connection) {
-  return db('meetings')
-    .insert(meeting)
+function getMeeting(meetingId, db = connection) {
+  return db("meetings")
+    .where("id", meetingId)
+    .then((meetings) => {
+      return Promise.all(
+        meetings.map((meeting) => {
+          return db("users")
+            .join("attendees", "attendees.user_id", "user.id")
+            .where("attendess.meeting_id", meetingId)
+            .select("users.id as user_Id", "users.username")
+            .then((users) => {
+              meeting.users = users;
+              return meeting;
+            });
+        })
+      );
+    })
+    .then( meeting => {
+      return meeting
+    })
 }
 
-function updateMeeting (meeting, db = connection) {
-  return db('meetings')
-    .where('id', meeting.id)
-    .update(meeting)
+function addMeeting(meeting, db = connection) {
+  return db("meetings").insert(meeting);
 }
 
-
+function updateMeeting(meeting, db = connection) {
+  return db("meetings").where("id", meeting.id).update(meeting);
+}
 
 module.exports = {
   getMeetings,
   getMeeting,
   addMeeting,
-  updateMeeting
-}
+  updateMeeting,
+};
