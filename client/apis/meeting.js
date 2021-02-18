83b88881dd2f043f2a4edf6592daf84f902cf253
student <student@eda-bc-09.localdomain>
2021-02-18 14:44:26 +1300

    added working checklist so you cant have duplicate attendees

--- client/apis/meeting.js
+++ client/apis/meeting.js
@@ -6,3 +6,9 @@ export function getUsers () {
   return request.get(baseUrl)
   .then(res => res.body)
 }
+
+export function addMeeting(meeting, userids) {
+  return request.post(baseUrl+"/meetings")
+  .send(meeting,userids)
+  .then(res => res.body)
+}
--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -4,7 +4,8 @@ import { fetchUsers } from '../actions/meeting'
 
 function Meeting(props) {
 	const [count, setCount] = useState(0)
-	const [buttonStart, setButtonStart] = useState(true)
+  const [buttonStart, setButtonStart] = useState(true)
+  const [attendees, setAttendees] = useState([])
 
 	useEffect(() => {
 		let interval = null
@@ -26,8 +27,17 @@ function Meeting(props) {
 		setButtonStart(!buttonStart)
   }
   
-  const checkBoxHandler = () => {
-    
+  const checkBoxHandler =  (user) => {
+    setAttendees(currentAttendees => {
+      let newArr = currentAttendees.filter(attendee => {
+        // console.log(attendee.id, user.id)
+        return user.id != attendee.id
+      })
+      // console.log(newArr)
+      return newArr.length == currentAttendees.length 
+      ?  [...newArr, user]
+      : [...newArr]
+    })
   }
 
 	return (
@@ -36,8 +46,8 @@ function Meeting(props) {
 				{props.users.map((u) => {
 					return (
 						<li key={u.id}>
-							<input type="checkbox" onClick={checkBoxHandler}></input>
-							{u.first_name} {u.last_name}
+							<input type="checkbox" onClick={() => checkBoxHandler(u)}></input>
+							{u.username} ({u.first_name} {u.last_name}) 
 						</li>
 					)
 				})}
